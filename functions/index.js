4e753c0005633f8b0bdd65d3b2c519a957cd4a2c
Poulami <poulami@DESKTOP-A9JAA71.(none)>
2020-09-13 22:34:31 +0530

    cloud functions done

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const vision = require("@google-cloud/vision");
-const express = require('express');
+const express = require("express");
 const cors = require("cors")({ origin: true });
 const app = express();
 
@@ -13,7 +13,7 @@ app.use(cors);
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 }); */
-app.post('/TextExtraction',async (req, res) => {
+app.post("/TextExtraction", async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   if (req.method !== "POST") {
     return res.status(403).send("Forbidden!");
@@ -47,41 +47,38 @@ app.post('/TextExtraction',async (req, res) => {
   });
 });
 
-
-exports.HandWrittenTextExtraction = functions.https.onRequest(
-  async (req, res) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    if (req.method !== "POST") {
-      return res.status(403).send("Forbidden!");
-    }
-    const { imageString } = req.body;
-    if (typeof imageString === "undefined" || imageString === "") {
-      return res.status(400).json({ status: "failed", message: "Bad request" });
-    }
-
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-
-    // Performs label detection on the local file
-    const request = {
-      image: {
-        content: imageString,
-      },
-      features: [
-        {
-          type: "DOCUMENT_TEXT_DETECTION",
-        },
-      ],
-    };
-
-    const [result] = await client.annotateImage(request);
-    const detection = result.textAnnotations[0].description.split("\n");
-
-    return res.status(200).json({
-      status: "success",
-      result: detection.slice(0, -1),
-    });
+app.post("/HandWrittenTextExtraction", async (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  if (req.method !== "POST") {
+    return res.status(403).send("Forbidden!");
   }
-);
+  const { imageString } = req.body;
+  if (typeof imageString === "undefined" || imageString === "") {
+    return res.status(400).json({ status: "failed", message: "Bad request" });
+  }
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the local file
+  const request = {
+    image: {
+      content: imageString,
+    },
+    features: [
+      {
+        type: "DOCUMENT_TEXT_DETECTION",
+      },
+    ],
+  };
+
+  const [result] = await client.annotateImage(request);
+  const detection = result.textAnnotations[0].description.split("\n");
+
+  return res.status(200).json({
+    status: "success",
+    result: detection.slice(0, -1),
+  });
+});
 
 exports.app = functions.https.onRequest(app);
